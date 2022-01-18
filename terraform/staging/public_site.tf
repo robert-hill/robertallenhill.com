51e737d3015037d1a2f442a9193e7db1a1bc73a4
Robert A. Hill <robert@decentlabs.com>
2022-01-18 10:51:44 -0800

    remove tests

--- terraform/staging/public_site.tf
+++ terraform/staging/public_site.tf
@@ -48,7 +48,3 @@ output "s3_bucket" {
   description = "Name of the S3 origin bucket"
   value       = module.cloudfront_s3_cdn.s3_bucket
 }
-
-resource "aws_s3_bucket" "test" {
-  name = "rhill-terraform-test"
-}
