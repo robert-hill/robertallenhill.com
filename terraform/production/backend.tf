0112851ba76bf433eeec24684cd3fd5c8f500ef7
Robert A. Hill <robert@decentlabs.com>
2022-01-18 20:27:51 -0800

    fmt goblin

--- terraform/production/backend.tf
+++ terraform/production/backend.tf
@@ -42,8 +42,8 @@ resource "aws_s3_bucket" "terraform" {
 resource "aws_s3_bucket_public_access_block" "example" {
   bucket = aws_s3_bucket.terraform.id
 
-  block_public_acls   = true
-  block_public_policy = true
-  ignore_public_acls  = true
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
   restrict_public_buckets = true
 }
