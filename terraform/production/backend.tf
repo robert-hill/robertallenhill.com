b6a176debe1ac2993364128a15e8624e76640795
Robert A. Hill II <robert@decentlabs.com>
2022-07-26 21:13:39 -0700

    Upgrade terraform (#60)
    * Upgrade terraform and provider.
    
    * workflow

--- terraform/production/backend.tf
+++ terraform/production/backend.tf
@@ -7,10 +7,10 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = ">= 3.64.0, < 4.0.0"
     }
   }
-  required_version = "1.1.5"
+  required_version = "1.2.5"
 }
 
 provider "aws" {
